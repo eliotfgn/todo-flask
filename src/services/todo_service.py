4b7a7eca5d1edf4d81740d4ce1f5a90695701459
Eliot Fagnon <efagnon2002@gmail.com>
2023-03-17 12:39:10 +0100

    remind todos

--- src/services/todo_service.py
+++ src/services/todo_service.py
@@ -1,5 +1,16 @@
+from datetime import timedelta
+
 from src.database import db
 from src.models.todo import Todo
+from src.scheduler import scheduler
+
+
+def remind(todo_id: int) -> None:
+    print(f"Reminder for todo with id: {todo_id}.")
+
+
+def remind_thirty_min_before(todo_id: int) -> None:
+    print(f"It remains 30 to complete task with id: {todo_id}")
 
 
 class TodoService:
@@ -11,6 +22,13 @@ class TodoService:
         except Exception as e:
             print(e)
             return False
+
+        if todo.remind:
+            scheduler.add_job(func=remind, trigger="date", args=[todo.id], run_date=todo.remind)
+
+        if todo.deadline:
+            remind_date = todo.deadline - timedelta(minutes=30)
+            scheduler.add_job(func=remind_thirty_min_before, trigger="date", args=[todo.id], run_date=remind_date)
         return True
 
     @staticmethod

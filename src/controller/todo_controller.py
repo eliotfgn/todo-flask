f8b7e6cfefb5b06fedbc37de99a535463a0aa7d5
Eliot Fagnon <efagnon2002@gmail.com>
2023-03-17 11:22:22 +0100

    add route for retrieving todo by id

--- src/controller/todo_controller.py
+++ src/controller/todo_controller.py
@@ -8,3 +8,8 @@ todo = Blueprint("todo", __name__, url_prefix="/api/v1/todos")
 @todo.get("/")
 def get_all():
     return jsonify(TodoService.get_all())
+
+
+@todo.get("/<int:todo_id>")
+def get_by_id(todo_id: int):
+    return TodoService.get_by_id(todo_id)

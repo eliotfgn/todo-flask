08255bfb7480312cf0f8db111dc63af0fa1b325e
Eliot Fagnon <efagnon2002@gmail.com>
2023-03-17 10:54:25 +0100

    create todo model

---
+++ src/models/todo.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime
+
+from src.database import db
+
+
+class Todo(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String)
+    description = Column(String)
+    deadline = Column(DateTime)
+    remind = Column(DateTime)
+    createdAt = Column(DateTime, default=datetime.now())
+    updatedAt = Column(DateTime, onupdate=datetime.now())
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'deadline': self.deadline,
+            'remind': self.remind,
+            'createdAt': self.createdAt,
+            'updatedAt': self.updatedAt
+        }

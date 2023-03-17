5a345494749df8c29ecf61472ef85867a4acee7a
Eliot Fagnon <efagnon2002@gmail.com>
2023-03-17 10:48:46 +0100

    init app with database session

--- src/app.py
+++ src/app.py
@@ -1,3 +1,11 @@
 from flask import Flask
+from database import db
+from src.config import Config
 
 app: Flask = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+
+if __name__ == '__main__':
+    app.run()

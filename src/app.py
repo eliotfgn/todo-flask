dd2b0672e4330a203a3157a2da0bcac3b415b165
Eliot Fagnon <efagnon2002@gmail.com>
2023-03-17 10:58:32 +0100

    register todo blueprint in the app

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 
+from src.controller.todo_controller import todo
 from src.database import db
 from src.config import Config
 
@@ -10,6 +11,8 @@ app.config.from_object(Config)
 db.init_app(app)
 
 migrate = Migrate(app, db)
+
+app.register_blueprint(todo)
 
 if __name__ == '__main__':
     app.run()

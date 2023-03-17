3f602df0ec2cf8dcd7388f1ec4ec41a8d0192488
Eliot Fagnon <efagnon2002@gmail.com>
2023-03-17 12:07:41 +0100

    initialize the scheduler object

--- src/app.py
+++ src/app.py
@@ -4,12 +4,13 @@ from flask_migrate import Migrate
 from src.controller.todo_controller import todo
 from src.database import db
 from src.config import Config
+from src.scheduler import scheduler
 
 app: Flask = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
-
+scheduler.start()
 migrate = Migrate(app, db)
 
 app.register_blueprint(todo)
